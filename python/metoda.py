24b630fe6e01e476bda27cfcdcefc1ffca836d4e
Slawomir <gliba@wp.pl>
2019-06-13 23:01:37 +0200

    test

---
+++ python/metoda.py
@@ -0,0 +1,13 @@
+def pogoda():
+    return 2*2
+
+print(pogoda())
+
+def pogoda2():
+    b=2*4
+    return b
+print(pogoda2())
+
+def przedstaw_sie(imie, wiek):
+    print(f"Czesc mam na imie {imie}, i mam {wiek} lat")
+przedstaw_sie("Slawek", 36)
--- python/petla_for.py
+++ python/petla_for.py
@@ -1,1 +1,2 @@
-ss
+for i in range(0, 20, 2):
+    print(i)
