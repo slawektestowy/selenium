db6fcf386e6424dd3bff692a6ea63643edfb2e9d
Slawomir <gliba@wp.pl>
2019-06-18 21:35:14 +0200

    newww

--- python/string_metody.py
+++ python/string_metody.py
@@ -2,3 +2,9 @@ imie = "Bartek"
 nazwisko = "Kowalski"
 adres = """Ulica Cwiartki 3/4
             00-001 Warszawa """
+
+print(imie.isupper())
+print(imie.upper(), imie.lower(),)
+print(imie[0])
+for i in imie:
+    print(i)
