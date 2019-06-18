d4e2dca48f1fa624313dc4dbe508a570869fa6c8
glibowsk <gliba@wp.pl>
2019-06-18 15:33:07 +0200

    nowe

---
+++ python/kalkulator.py
@@ -0,0 +1,14 @@
+def dodawanie(pierwszza, druga):
+    return pierwszza + druga
+
+def odejmowanie(pierwsza, druga):
+    return pierwsza - druga
+
+def mnozenie(pierwsza, druga):
+    return pierwsza * druga
+
+def dzielenie(pierwsza, druga):
+    return pierwsza / druga
+
+
+print(dodawanie(3,2),'\n',mnozenie(3,2),'\n',odejmowanie(10,5),'\n', dzielenie(10,5) )
