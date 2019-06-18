44a426124f5dd5ffb9ee85bfcdc41389d4233a29
glibowsk <gliba@wp.pl>
2019-06-18 13:42:35 +0200

    nowe

--- python/args.py
+++ python/args.py
@@ -1,23 +1,24 @@
-def rzeczy(*args):
-    for i in args:
-        print(i)
-
-rzeczy('litera', "string", "float")
-
-print("$" * 30)
-
-def rzeczy(pierwsza_rzecz, *args):
-    print(pierwsza_rzecz)
-    print(args[0])
-    for i in args:
-        print(i)
-
-rzeczy('litera', "string", "float", "number")
+# def rzeczy(*args):
+#     for i in args:
+#         print(i)
+#
+# rzeczy('litera', "string", "float")
+#
+# print("$" * 30)
+#
+# def rzeczy(pierwsza_rzecz, *args):
+#     print(pierwsza_rzecz)
+#     print(args[0])
+#     for i in args:
+#         print(i)
+#
+# rzeczy('litera', "string", "float", "number")
 
 
 def dziennik(klasa, **kwargs):
     print(f'Klasa {klasa}')
-    for nazwisko in kwargs.values():
+    for nazwisko in kwargs.keys():
         print(nazwisko)
+    print(kwargs.get('Kucharski'))
 
 dziennik('3A', Kowalski = "1", Nowak = "2", Kucharski = "3")
