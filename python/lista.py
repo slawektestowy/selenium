85027f73c312af5fadcafac53bca54965fb7a8fa
Slawomir <gliba@wp.pl>
2019-06-18 22:23:31 +0200

    newww

---
+++ python/lista.py
@@ -0,0 +1,7 @@
+lista = ["Bartek", "Tomek", "Basia", 1, 2, 3, 4 ]
+
+print(lista[2])
+for i in lista:
+    print(i)
+
+    
--- python/string_metody.py
+++ python/string_metody.py
@@ -8,3 +8,6 @@ print(imie.upper(), imie.lower(),)
 print(imie[0])
 for i in imie:
     print(i)
+print(imie[0:4])
+print("lal".join(["cos", "z",  "czyms"]))
+print("aaa a aaa aaa aaaa aaaaaaa".split())
