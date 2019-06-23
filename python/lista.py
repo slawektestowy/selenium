b329f015d26196d892052737990a5c442ec23fbd
Slawomir <gliba@wp.pl>
2019-06-23 23:01:19 +0200

    new lista

--- python/lista.py
+++ python/lista.py
@@ -4,4 +4,17 @@ print(lista[2])
 for i in lista:
     print(i)
 
-ss
+print(lista[0:4])
+print(lista.index("Tomek"))   # miejsce na ktorym znajduje sie dany element listy
+
+lista.append("Jedynka") # dodanie elementu na koncu listy
+lista.insert(0, "nova") # dodanie elementu na konkretnym miejscu listty
+print(lista)
+
+print(len(lista)) # sprawdzenie dlugosci listy
+
+lista.remove("Basia")
+print(lista)
+
+del lista[0]
+print(lista)
