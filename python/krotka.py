a281df56d0e1f275d9cd954efdd342fff7781184
glibowsk <gliba@wp.pl>
2019-06-25 10:02:28 +0200

    nowe

--- python/krotka.py
+++ python/krotka.py
@@ -1,2 +1,3 @@
 jezyk_programowania = ("java", "php", "c++", "go")
 print(jezyk_programowania[1])
+print(jezyk_programowania[0:2])
---
+++ python/zbiory.py
@@ -0,0 +1,9 @@
+pierwszy_zbior = {"Wroclaw", "Warszawa", "Konin"}
+drugi_zbior = {"Warszawa"}
+
+
+pierwszy_zbior.add("Torun")
+print(pierwszy_zbior)
+pierwszy_zbior.add("Konin") # nie doda elementu do zbioru, ktory juz istnieje
+print(pierwszy_zbior)
+
