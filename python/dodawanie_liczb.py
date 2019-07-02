33426cb83256d85c22a8a710b93a3a7abdb25365
Slawomir <gliba@wp.pl>
2019-07-02 22:37:56 +0200

    new

--- python/dodawanie_liczb.py
+++ python/dodawanie_liczb.py
@@ -1,18 +1,28 @@
-try:
-    print("Podaj pierwsza liczbe: ")
-    pierwsza_liczba = int(input())
-    print("Podaj druga liczbe: ")
-    druga_liczba = int(input())
-    print(f"Twoj wynik, ktory uzyskales to {pierwsza_liczba + druga_liczba} ")
-except ValueError:
-    print("Nie podałeś liczby ani cyfry")
+# try:
+#     print("Podaj pierwsza liczbe: ")
+#     pierwsza_liczba = int(input())
+#     print("Podaj druga liczbe: ")
+#     druga_liczba = int(input())
+#     print(f"Twoj wynik, ktory uzyskales to {pierwsza_liczba + druga_liczba} ")
+# except ValueError:
+#     print("Nie podałeś liczby ani cyfry")
 
 
-def Niedzielnik(a, b):
-    try:
-        return a/b
-    except ZeroDivisionError:
-        return("nie mozna dzielic przez ZERO")
+def Niedzielnik():
+    while True:
+        try:
+            print("poda pierwsza liczbe: ")
+            a = int(input())
+            print("poda druga liczbe: ")
+            b = int(input())
+            print (a / b)
+            break
+        except ZeroDivisionError:
+            print("nie mozna dzielic przez ZERO")
+            continue
+        except ValueError:
+            print("nie moga byc stringi")
+            continue
 
-print(Niedzielnik(1,0))
+Niedzielnik()
 
