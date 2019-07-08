d5326605977bfded4df9ddeb3cf11bce4ca4b8d7
Slawomir <gliba@wp.pl>
2019-07-08 23:46:02 +0200

    sa

--- python/dziedziczenie.py
+++ python/dziedziczenie.py
@@ -13,6 +13,9 @@ class pozycja(pracownik):
         pracownik.__init__(self,imie,nazwisko)
         self.poz = poz
 
+    def podaje_pozycje(self):
+        return ("Moja pozycja to " + self.poz)
+
 
 
 pracownik1 = pracownik("John", "Rambo")
@@ -20,3 +23,4 @@ print(pracownik1.nazwisko)
 
 pracownik2 = pozycja("Jan", "Burak", "Manager")
 print(pracownik2.przedstaw_sie())
+print(pracownik2.podaje_pozycje())
