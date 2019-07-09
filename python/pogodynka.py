73519536b5c38e638a19c124376b0e083583beb2
Slawomir <gliba@wp.pl>
2019-07-09 13:53:11 +0200

    ss

---
+++ python/pogodynka.py
@@ -0,0 +1,15 @@
+def pogoda_wroclaw():
+    return "pochmurnie"
+
+
+def pogoda_krakow():
+    return "Slonecznie"
+
+
+def pogoda_warszawa():
+    return "Pada deszcz"
+
+
+print(pogoda_krakow())
+print(pogoda_wroclaw())
+print(pogoda_warszawa())
---
+++ python/sprawdz_pogode.py
@@ -0,0 +1,5 @@
+import pogodynka
+
+print("Witaj w prognozie pogody")
+print(f"Dzis w Krakoiwe jest {pogodynka.pogoda_krakow()}")
+print("We Wroclawiu natomiast jest " + pogodynka.pogoda_wroclaw())
